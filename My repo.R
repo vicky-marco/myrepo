ed92f77547a7e3f3063d97e0d5b722b35eed0754
vicky-marco <vicky_marco@hotmail.com>
2021-06-29 01:14:01 -0300

    Avances

--- My repo.R
+++ My repo.R
@@ -2,30 +2,30 @@
 
 #Probando diferentes formas de importar datos
 
-of_gastro <-read.csv(file ="oferta_gastronomica.csv")
+delitos_2020 <-read.csv(file ="delitos_badata_2020.csv")
 
 library(readr)
-of_gastro2 <- read_csv(file = "oferta_gastronomica.csv")
-
-library(readxl)
-of_gastro3 <- read_xlsx(path = "oferta_gastronomica.xlsx")
+delitos_2020B <- read_csv(file = "delitos_badata_2020.csv")
 
 library(vroom)
-of_gastro4 <- vroom("oferta_gastronomica.csv", delim = ",")
+delitos_2020C <- vroom("delitos_badata_2020.csv", delim = ",")
 
-as_tibble(of_gastro2)
+#Conociendo el dataset
+as_tibble(delitos_2020B)
 
-head(of_gastro2)
+head(delitos_2020B)
 
-str(of_gastro2)
+str(delitos_2020B)
+
+#Ordenando la info
 
 library(tidyverse)
 library(tidyr)
 
-of_gastro2 %>% 
-  pivot_longer(cols = c(telefono, mail),
-               names_to = "restaurants",
-               values_to = "info")
+delitos_2020B %>% 
+  pivot_wider(names_from = "comuna",
+              values_from = "victimas")
+
   
   
   
