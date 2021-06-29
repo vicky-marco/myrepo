ae93bc56680db19029d2d0c326e2f03afb6ebb7c
vicky-marco <vicky_marco@hotmail.com>
2021-06-29 00:59:50 -0300

    Avances

--- My repo.R
+++ My repo.R
@@ -13,11 +13,21 @@ of_gastro3 <- read_xlsx(path = "oferta_gastronomica.xlsx")
 library(vroom)
 of_gastro4 <- vroom("oferta_gastronomica.csv", delim = ",")
 
+as_tibble(of_gastro2)
+
 head(of_gastro2)
 
 str(of_gastro2)
 
 library(tidyverse)
+library(tidyr)
 
-tidyr::pivot_wider(of_gastro2)
-tidyr::pivot_longer(of_gastro2)
+of_gastro2 %>% 
+  pivot_longer(cols = c(telefono, mail),
+               names_to = "restaurants",
+               values_to = "info")
+  
+  
+  
+  
+  
